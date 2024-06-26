75e1360534476e0a08dfbf927df33305d6536a6e
Maxim <m.glushchenko@office.hoster.kz>
2024-06-26 16:46:38 +0500

    Fix that script sometimes grabs not mail

--- pyScrs/fmail.py
+++ pyScrs/fmail.py
@@ -4,7 +4,8 @@ parser = argparse.ArgumentParser(description='Returns list of senders by recipie
 parser.add_argument("to", type=str, help="email address of recipient")
 args=parser.parse_args()
 for line in sys.stdin:
-    if f"to=<{args.to}>" in line and "MAILER-DAEMON" not in line:
+    pattern=fr".*from.*{args.to}"
+    if bool(re.search(pattern, line)) and "MAILER-DAEMON" not in line:
         m =re.search(r"(?<=<).+?(?=>)",line) 
         if m:
             sys.stdout.write(m.group()+"\n")
