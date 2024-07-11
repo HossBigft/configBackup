3f3d925b22c0be741d80eaa2bc976bccf068afa6
Maxim <m.glushchenko@office.hoster.kz>
2024-07-11 13:54:52 +0500

    Add log and saving functionality to pVersionList function

--- fish/functions/pMailmanVersionList.fish
+++ fish/functions/pMailmanVersionList.fish
@@ -1,7 +1,7 @@
 function pMailmanVersionList
     set currDate (date +%Y%m%d_%H%M)
     echo "Starting queries"
-    mkdir -p ~/pkzStats && touch ~/pkzStats/pleskMailmanVersions$currDate
+    mkdir -p ~/pkzStats && touch ~/pkzStats/pleskMailmanVersions$currDate.txt
     for i in (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}\$")
         set ver (ssh maximg@$i 'rpm -q mailman')
         echo $i $ver
--- fish/functions/pVersionList.fish
+++ fish/functions/pVersionList.fish
@@ -1,6 +1,11 @@
 function pVersionList
+    set currDate (date +%Y%m%d_%H%M)
+    echo "Starting queries"
+    mkdir -p ~/pkzStats && touch ~/pkzStats/pleskVersions$currDate.txt
     for i in (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}\$")
-        set pleskVersion (ssh maximg@$i 'plesk -v'|grep -Po "Plesk.*");
-        echo $i $pleskVersion;
-    end;
+        set pleskVersion (ssh maximg@$i 'plesk -v'|grep -Po "Plesk.*")
+        echo $i $pleskVersion
+        echo $i $pleskVersion >> ~/pkzStats/pleskVersions$currDate.txt
+    end
+    echo "Saved in ~/pkzStats/pleskVersions$currDate.txt"
 end
