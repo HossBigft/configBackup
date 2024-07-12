27637b2d75ff63878591347b4afbf66786b85781
Maxim <m.glushchenko@office.hoster.kz>
2024-07-12 14:10:57 +0500

    Add function rmZoneMaster -- it removes zone data about domain from dns servers

---
+++ fish/functions/rmZoneMaster.fish
@@ -0,0 +1,7 @@
+function rmZoneMaster
+set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
+    for host in $dnsServers
+        echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
+        ssh root@$host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
+    end
+end
