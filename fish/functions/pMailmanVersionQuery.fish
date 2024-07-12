79af060d5733d869b9f5c482327bac2b8e2a6e48
Maxim <m.glushchenko@office.hoster.kz>
2024-07-12 16:39:36 +0500

    Rename pMailmanVersionList>pMailmanVersionQuery and use variables instead of plain text

---
+++ fish/functions/pMailmanVersionQuery.fish
@@ -0,0 +1,20 @@
+function pMailmanVersionQuery
+    set currDate (date +%Y%m%d_%H%M)
+    set filename pleskMailmanVersions
+    set pleskServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}\$")
+
+    echo "Starting query {rpm -q mailman}"
+
+    mkdir -p ~/pkzStats && touch ~/pkzStats/$filename$currDate.txt
+
+    for host in $pleskServers
+        set ver (ssh maximg@$host 'rpm -q mailman')
+        echo $host $ver
+        echo $host $ver >> ~/pkzStats/$filename$currDate.txt
+    end
+    
+    echo Sorting
+    sort -t' ' -k2 -o ~/pkzStats/$filename$currDate.txt{,}
+
+    echo "Saved in ~/pkzStats/$filename$currDate.txt"
+end
