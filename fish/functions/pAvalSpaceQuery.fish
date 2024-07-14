1bb867a11ccb9332e1615b75391438f606686974
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-07-14 13:42:33 +0500

    Fix pAvalSpaceQuery edge case when main volume is /var, not /

--- fish/functions/pAvalSpaceQuery.fish
+++ fish/functions/pAvalSpaceQuery.fish
@@ -1,13 +1,18 @@
 function pAvalSpaceQuery
     set currDate (date +%Y%m%d_%H%M)
     set filename pleskAvailableSpace
+    set servers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
+
+    
     echo "Starting queries"
     mkdir -p ~/pkzStats && touch ~/pkzStats/$filename$currDate.txt
-    for i in (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
-        set avalSpace (ssh maximg@$i 'df -h'|grep -P ".*\/\$")
-        echo $i $avalSpace
-        echo $i $avalSpace | tr -s ' ' >>~/pkzStats/$filename$currDate.txt
+    
+    for server in servers
+        set response (ssh maximg@$server 'df -h')
+        set extractedValue (echo $response | tr -s ' ' | string join "; " | grep -Po "(?:\S+\s+){5}\/var;|(?:\S+\s+){5}\/(?!.*\/var);")
+        echo $extractedValue && echo $extractedValue >>~/pkzStats/$filename$currDate.txt
     end
+    
     echo Sorting
     sort -t' ' -k6 -r -o ~/pkzStats/$filename$currDate.txt{,}
     echo "Saved in ~/pkzStats/$filename$currDate.txt"
