8636e6ef60e3ed560bbb8368034d89dab759bd9a
Maxim <m.glushchenko@office.hoster.kz>
2024-07-16 17:23:13 +0500

    Rename rmZoneMaster.fish -> nsRmZoneMaster.fish

--- fish/functions/nsRmZoneMaster.fish
+++ fish/functions/nsRmZoneMaster.fish
@@ -1,4 +1,4 @@
-function rmZoneMaster
+function nsRmZoneMaster
 set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
     for host in $dnsServers
         echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
