dc9047820da3c77a8a886ac65d7de5b16024e409
Maxim <m.glushchenko@office.hoster.kz>
2024-07-16 16:39:00 +0500

    Add function pFindSameVersionOrGreater, it takes Plesk version and prints list of migration compatible servers

---
+++ fish/functions/pFindSameVersionOrGreater.fish
@@ -0,0 +1,15 @@
+function pFindSameVersionOrGreater
+    set targetVersion (echo $argv)
+    set targetVersionNumber (echo $targetVersion | string split '.' | string join '')
+    echo $targetVersionNumber
+    set serverData (fish -c pGetVersionsList)
+
+    for server in $serverData
+        set serverVersion (echo $server | grep -Po "\d+(\.\d+)+")
+        set serverVersionNumber (echo $serverVersion | string split '.' | string join '')
+        if test $serverVersionNumber -ge $targetVersionNumber
+            set serverName (echo $server | grep -Po "^([^.])+")
+            echo $serverName\| $serverVersion \>\= $targetVersion
+        end
+    end
+end
