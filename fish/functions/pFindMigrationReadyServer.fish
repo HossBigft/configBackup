6c48cbd3502c9472546c79558a7076a361d46efc
Maxim <m.glushchenko@office.hoster.kz>
2024-07-16 17:59:29 +0500

    Start making function pFindMigrationReadyServer--- it should accept plesk version and amount in gb to provide list of migration ready servers

---
+++ fish/functions/pFindMigrationReadyServer.fish
@@ -0,0 +1,8 @@
+function pFindMigrationReadyServer
+    set targetVersion (echo $argv[1])
+    echo $targetVersion
+    set subscriptionSize (echo $argv[2])
+    set listOfVersionCompatibleServer (fish -c pFindSameVersionOrGreater|echo $targetVersion|psub)
+    echo $listOfVersionCompatibleServer
+    
+end
