f14f1abbcd7c176855024f3855b8feb4ba4bf933
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-07-16 21:34:12 +0500

    Rename pFindMigrationReadyServer.fish -> pCheckMigrationVersion.fish

--- fish/functions/pCheckMigrationVersion.fish
+++ fish/functions/pCheckMigrationVersion.fish
@@ -1,8 +1,7 @@
-function pFindMigrationReadyServer
+function pCheckMigrationVersion
     set targetVersion (echo $argv[1])
     echo $targetVersion
     set subscriptionSize (echo $argv[2])
     set listOfVersionCompatibleServer (fish -c pFindSameVersionOrGreater|echo $targetVersion|psub)
     echo $listOfVersionCompatibleServer
-    
 end
