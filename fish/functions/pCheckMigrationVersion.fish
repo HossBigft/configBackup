abb596e8710a7bf9ecb4d57335b3a85d53f4faac
Maxim <m.glushchenko@office.hoster.kz>
2024-07-17 08:54:19 +0500

    Make pCheckMigrationSpace output hsots delimited by pipe symbol

--- fish/functions/pCheckMigrationVersion.fish
+++ fish/functions/pCheckMigrationVersion.fish
@@ -1,6 +1,8 @@
 function pCheckMigrationVersion
     set targetVersion (echo $argv[1])
     set subscriptionSize (echo $argv[2])
-    set listOfVersionCompatibleServer (fish -c "pFindSameVersionOrGreater $targetVersion")
-    echo $listOfVersionCompatibleServer
+    set listOfVersionCompatibleServers (fish -c "pFindSameVersionOrGreater $targetVersion" | grep -Po "^([^|])+")
+    set listOfSpaceFittingServers (fish -c "pCheckMigrationSpace $subscriptionSize")
+    echo $listOfVersionCompatibleServers
+    echo $listOfSpaceFittingServers
 end
