4b6a289a533131c073ad23062d4ab148cdc9b2a3
Maxim <m.glushchenko@office.hoster.kz>
2024-07-17 14:05:49 +0500

    Add working version pCheckMigrationVersion

--- fish/functions/pCheckMigrationVersion.fish
+++ fish/functions/pCheckMigrationVersion.fish
@@ -1,14 +1,19 @@
 function pCheckMigrationVersion
     set targetVersion (echo $argv[1])
     set subscriptionSize (echo $argv[2])
-    set listOfVersionCompatibleServernames (fish -c "pFindSameVersionOrGreater $targetVersion" | grep -Po "^([^|])+")
-    set listOfSpaceFittingServers (fish -c "pCheckMigrationSpace $subscriptionSize" )
+    set listOfVersionCompatibleServers (fish -c "pFindSameVersionOrGreater $targetVersion")
+    set listOfVersionCompatibleServernames (string collect $listOfVersionCompatibleServers | grep -Po "^([^|])+")
+    set listOfSpaceFittingServers (fish -c "pCheckMigrationSpace $subscriptionSize")
     set listOfSpaceFittingServernames
+    
     for line in $listOfSpaceFittingServers
         set -a listOfSpaceFittingServernames  (echo $line | grep -Po "^([^|])+")
     end
-    set listOfVersionSpaceCompatibleServers  (echo $listOfSpaceFittingServers | grep -P (string join '|' $listOfVersionCompatibleServernames))
-    for server in (string split ';' (string replace -a '; ' ';' $listOfVersionSpaceCompatibleServers))
-        echo $server
+    
+    set regexPatternForVersionCompatibleServerNames (string join '|' $listOfVersionCompatibleServernames)
+    set listOfVersionSpaceCompatibleServers  (string split -n ';' $listOfSpaceFittingServers | grep -P $regexPatternForVersionCompatibleServerNames )
+    
+    for server in (string split -n ';' (string replace -a '; ' ';' $listOfVersionSpaceCompatibleServers))
+        echo $server\;
     end
 end
