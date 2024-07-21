7ad95f8fc2aaa14132df34d26b2a58fd73196ee5
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-07-21 09:44:59 +0500

    Add functionality to find last file in dir for pFindMigrationReadyServer

--- pyScrs/pFindMigrationReadyServer.py
+++ pyScrs/pFindMigrationReadyServer.py
@@ -1,8 +1,9 @@
-import subprocess,re
-# Run a simple shell command
-# Capture the output of a command
+import subprocess,re, pathlib
+userHomeDir=pathlib.Path.home()
+pathlib.Path(f"{userHomeDir}/pkzStats").mkdir(parents=True, exist_ok=True)
 result = subprocess.run(["df", "-BG"],capture_output=True, text=True)
-#print(result.stdout)
+print(pathlib.Path(f"{userHomeDir}/pkzStats").resolve())
+print(list(pathlib.Path(f"{userHomeDir}/pkzStats").glob("pleskAvailableSpace*"))[-1])
 result = result.stdout.splitlines()
 result = [" ".join(line.split()) for line in result]
 result = "".join(filter(lambda s: re.fullmatch("(?:\S+\s+){5}/var;|((?:\S+\s+){5}/)(?!.*/var)",s),result))
