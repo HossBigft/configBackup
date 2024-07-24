622bd55669ac81ef04ff17a45a27e4a401f75244
Maxim <m.glushchenko@office.hoster.kz>
2024-07-24 14:38:46 +0500

    Add function pMailQueueSizeQuery that queries server for mail queue size

---
+++ fish/functions/pMailQueueSizeQuery.fish
@@ -0,0 +1,20 @@
+function pMailQueueSizeQuery
+    set currDate (date +%Y%m%d_%H%M)
+    set filename pleskMailQueueSize
+    set servers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
+
+    
+    echo "Starting queries"
+    mkdir -p ~/pkzStats && touch ~/pkzStats/$filename$currDate.txt
+    
+    for server in $servers
+        echo "Querying $server"
+        set response (ssh maximg@$server "mailq | grep -c '^[A-F0-9]'")
+        echo "$server  answered $response"
+        echo $server\; Mail queue size $response >>~/pkzStats/$filename$currDate.txt
+    end
+    
+    echo Sorting
+    sort -t' ' -n -k5 -o ~/pkzStats/$filename$currDate.txt{,}
+    echo "Saved in ~/pkzStats/$filename$currDate.txt"
+end
