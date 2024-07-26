7a301632cc2312afdc0442f7e12a9ab48edc6410
Maxim <m.glushchenko@office.hoster.kz>
2024-07-26 14:59:11 +0500

    Rename pVersionList.fish -> pVersionQuery.fish and add variables

--- fish/functions/pVersionQuery.fish
+++ fish/functions/pVersionQuery.fish
@@ -1,13 +1,14 @@
-function pVersionList
+function pVersionQuery
     set currDate (date +%Y%m%d_%H%M)
+    set filename pleskServerVersionList
     echo "Starting queries"
-    mkdir -p ~/pkzStats && touch ~/pkzStats/pleskVersions$currDate.txt
+    mkdir -p ~/pkzStats && touch ~/pkzStats/$filename$currDate.txt
     for i in (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}\$")
         set pleskVersion (ssh maximg@$i 'plesk -v'|grep -Po "Plesk.*")
         echo $i $pleskVersion
-        echo $i $pleskVersion >> ~/pkzStats/pleskVersions$currDate.txt
+        echo $i $pleskVersion >> ~/pkzStats/$filename$currDate.txt
     end
     echo "Sorting by version"
-    sort -k4 -o ~/pkzStats/pleskVersions$currDate.txt{,}
-    echo "Saved in ~/pkzStats/pleskVersions$currDate.txt"
+    sort -k4 -o ~/pkzStats/$filename$currDate.txt{,}
+    echo "Saved in ~/pkzStats/$filename$currDate.txt"
 end
