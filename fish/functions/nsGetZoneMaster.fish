a9e322c76e5fe1e09e1be9f6630dea1b2178c1b8
Maxim <m.glushchenko@office.hoster.kz>
2024-07-26 08:31:40 +0500

    Rename nsGetDomainZoneMaster.fish -> nsGetZoneMaster.fish and add reverse dns query in output

--- fish/functions/nsGetZoneMaster.fish
+++ fish/functions/nsGetZoneMaster.fish
@@ -1,8 +1,8 @@
-function nsGetDomainZoneMaster
+function nsGetZoneMaster
     set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
     echo Querying Hoster.KZ DNS servers about $argv
     for host in $dnsServers
         set zoneMaster (ssh root@$host "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf"| grep \"$argv\" | grep -Po "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
-        echo (echo $host|grep -Po "^([^.])+")  $zoneMaster
+        echo (echo $host|grep -Po "^([^.])+")\|  $zoneMaster\| (fish -c "digsx $zoneMaster")
     end
 end
