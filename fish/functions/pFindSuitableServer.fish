0249a7055f61b91f82be2e486fbc5bc8a25bf4c9
Maxim <m.glushchenko@office.hoster.kz>
2024-07-29 17:28:39 +0500

    pFindSuitableServer: change to python variant and save fish variant as LEGACY_pFindSuitableServer

--- fish/functions/pFindSuitableServer.fish
+++ fish/functions/pFindSuitableServer.fish
@@ -1,25 +1,3 @@
 function pFindSuitableServer
-    set targetVersion (echo $argv[1])
-    set subscriptionSize (echo $argv[2])
-    set listOfVersionCompatibleServers (fish -c "pFindSameVersionOrGreater $targetVersion")
-    set listOfVersionCompatibleServernames (string collect $listOfVersionCompatibleServers | grep -Po "^([^|])+")
-    set listOfSpaceFittingServers (fish -c "pCheckMigrationSpace $subscriptionSize")
-    set listOfSpaceFittingServernames
-    
-    for line in $listOfSpaceFittingServers
-        set -a listOfSpaceFittingServernames  (echo $line | grep -Po "^([^|])+")
-    end
-    
-    set regexPatternForVersionCompatibleServerNames (string join '|' $listOfVersionCompatibleServernames)
-    set listOfCompatibleServers (string split -n ';' $listOfSpaceFittingServers | grep -Pw $regexPatternForVersionCompatibleServerNames )
-    
-    set listOfCompatibleServerNames ( string collect $listOfCompatibleServers | grep -Po "^([^|])+" )
-    set listOfCompatibleVersions (string collect $listOfVersionCompatibleServers | grep -Pw (string join '|' $listOfCompatibleServerNames) | grep -Po "\d+(\.\d+)+ >= \d+(\.\d+)+;")
-
-
-    set counter 1
-    for server in (string split -n ';' (string replace -a '; ' ';' $listOfCompatibleServers))
-        echo $server S\>\=T $listOfCompatibleVersions[$counter]
-        set counter (math $counter+1)
-    end
+  python3  ~/configBackup/pyScrs/pFindMigrationReadyServer.py $argv
 end
