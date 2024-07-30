64e95e67a873e41a4699a687d3ef66349625a948
Maxim <m.glushchenko@office.hoster.kz>
2024-07-30 11:54:54 +0500

    nsGetSubDomains: add quiet option, that shows only domains

--- fish/functions/nsGetSubDomains.fish
+++ fish/functions/nsGetSubDomains.fish
@@ -1,5 +1,12 @@
 function nsGetSubDomains
+    set -l options q/quiet
+    argparse -n nsGetSubDomains $options -- $argv
+    or return
     set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
+    if set -q _flag_quiet
+        ssh root@$dnsServers[1] "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf" | grep $argv | grep -Po "(?<=\").*?(?=\" )"
+        return 0
+    end
     echo Querying Hoster.KZ DNS servers about $argv
     for host in $dnsServers
         echo (echo $host|grep -Po "^([^.])+")\|
