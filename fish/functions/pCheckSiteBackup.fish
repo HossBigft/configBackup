28964affb19ef4b39c909ba6755c628a74780a74
Maxim <m.glushchenko@office.hoster.kz>
2024-07-31 12:04:20 +0500

    Add pCheckSiteBackup: returns backup files if site backup is present

---
+++ fish/functions/pCheckSiteBackup.fish
@@ -0,0 +1,5 @@
+function pCheckSiteBackup --wraps==ssh --wraps=ssh
+    set host (echo $argv[1])
+    set domain (echo $argv[2])
+    ssh $host "find  /backup* -maxdepth 6 -type d -name $domain|xargs ls"  
+end
