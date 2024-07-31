a07b661d780e129c657e8c4d678874c2bb286d97
Maxim <m.glushchenko@office.hoster.kz>
2024-07-31 08:43:06 +0500

    Rename fish/functions/pMailmanVersionQuery.fish -> fish/functions/pQueryMailmanVersion.fish

--- fish/functions/pQueryMailmanVersion.fish
+++ fish/functions/pQueryMailmanVersion.fish
@@ -1,4 +1,4 @@
-function pMailmanVersionQuery
+function pQueryMailmanVersion
     set currDate (date +%Y%m%d_%H%M)
     set filename pleskMailmanVersions
     set pleskServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}\$")
