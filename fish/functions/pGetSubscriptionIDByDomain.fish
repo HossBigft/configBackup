dac3f6f7dd377559a83312bb5c753bc9270289ca
Maxim <m.glushchenko@office.hoster.kz>
2024-08-01 17:37:12 +0500

    pGetSubscriptionIDByDomain: fix case, when there is no line with single 0 in it

--- fish/functions/pGetSubscriptionIDByDomain.fish
+++ fish/functions/pGetSubscriptionIDByDomain.fish
@@ -2,6 +2,6 @@ function pGetSubscriptionIDByDomain --wraps=ssh
     set host (echo $argv[1])
     set domain (echo $argv[2])
 
-    ssh $host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\""|grep -v 0| sort -u
+    ssh $host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
     
 end
