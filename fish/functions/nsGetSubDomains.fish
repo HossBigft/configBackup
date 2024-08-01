3878d5972606ec07f8300b79ccfa64ee139aa777
Maxim <m.glushchenko@office.hoster.kz>
2024-08-01 09:02:36 +0500

    nsGetSubDomains: remove quiet option and move previus functionality to verbose flag, now it outputs list of subdomains

--- fish/functions/nsGetSubDomains.fish
+++ fish/functions/nsGetSubDomains.fish
@@ -1,15 +1,18 @@
 function nsGetSubDomains
-    set -l options q/quiet
+    set -l options v/verbose
     argparse -n nsGetSubDomains $options -- $argv
     or return
+    set domain (echo $argv|string lower)
     set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
-    if set -q _flag_quiet
-        ssh root@$dnsServers[1] "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf" | grep $argv | grep -Po "(?<=\").*?(?=\" )"
+    
+    if set -q _flag_verbose
+        echo Querying Hoster.KZ DNS servers about $argv
+        for host in $dnsServers
+            echo (echo $host|grep -Po "^([^.])+")\|
+            ssh root@$host "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf" | grep $domain | grep -Po "(?<=\").*?(?=\" )"
+        end
         return 0
     end
-    echo Querying Hoster.KZ DNS servers about $argv
-    for host in $dnsServers
-        echo (echo $host|grep -Po "^([^.])+")\|
-        ssh root@$host "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf" | grep $argv | grep -Po "(?<=\").*?(?=\" )"
-    end
+    
+    ssh root@$dnsServers[1] "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf" | grep $domain | grep -Po "(?<=\").*?(?=\" )"
 end
