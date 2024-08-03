407c3197e461f3df417ba0085d4386979fcc358b
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-08-03 10:49:27 +0500

    Digs: fix wrong argument count

--- fish/functions/digs.fish
+++ fish/functions/digs.fish
@@ -1,19 +1,18 @@
 function digs
-    set argsLen (echo $argv|string length)
-    
+    set argsLen (count $argv)
     if test $argsLen -eq 1 
-        set answer (dig @8.8.8.8 +short $argv)
+        set answer (dig +short @8.8.8.8  $argv)
         if test -n "$answer"
-            echo $answer
+            printf %s\n  $answer
             return 0
         else
             echo No result for $argv
             return 1
         end
     else
-        set answer (dig +short $argv)
+        set answer (dig +short  $argv)
         if test -n "$answer"
-            echo $answer
+            printf %s\n  $answer
             return 0
         else
             echo No result for $argv
