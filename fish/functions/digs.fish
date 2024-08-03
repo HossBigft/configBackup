3cc6e11483d34a3c74ce171e054e0da16526446c
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-08-03 10:03:29 +0500

    Digs: spedup dig with wrong domain by querying google dns. Also added outrup and return code

--- fish/functions/digs.fish
+++ fish/functions/digs.fish
@@ -1,3 +1,23 @@
 function digs
- dig +time=5 +tries=1 +short $argv; 
+    set argsLen (echo $argv|string length)
+    
+    if test $argsLen -eq 1 
+        set answer (dig @8.8.8.8 +short $argv)
+        if test -n "$answer"
+            echo $answer
+            return 0
+        else
+            echo No result for $argv
+            return 1
+        end
+    else
+        set answer (dig +short $argv)
+        if test -n "$answer"
+            echo $answer
+            return 0
+        else
+            echo No result for $argv
+            return 1
+        end
+    end
 end
