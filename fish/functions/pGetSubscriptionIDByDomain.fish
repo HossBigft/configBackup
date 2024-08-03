cba1eed10baa6a1fd42e6a3e121401be972cdb10
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-08-03 14:21:00 +0500

    pGetSubscriptionIDByDomain: now tries to find host from ptr record, if host is not given

--- fish/functions/pGetSubscriptionIDByDomain.fish
+++ fish/functions/pGetSubscriptionIDByDomain.fish
@@ -1,7 +1,16 @@
 function pGetSubscriptionIDByDomain --wraps=ssh
-    set host (echo $argv[1])
-    set domain (echo $argv[2])
-
-    ssh $host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
+    set argNum (count $argv)
     
+    if test $argNum -eq 1
+        set domain (echo $argv)
+        set host (digs $domain|string sub -e-1)
+
+        ssh $host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
+        
+    else if test $argNum -eq 2
+        set host (echo $argv[1])
+        set domain (echo $argv[2])
+
+        ssh $host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
+    end
 end
