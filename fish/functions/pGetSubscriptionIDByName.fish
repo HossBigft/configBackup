b75529378a0af0375c682354ced8cd3e37cf0f90
Maxim <m.glushchenko@office.hoster.kz>
2024-08-05 17:16:13 +0500

    Remove pGetSubscriptionIDByName, not sure why I added it

--- fish/functions/pGetSubscriptionIDByName.fish
+++
@@ -1,6 +0,0 @@
-function pGetSubscriptionIDByName --wraps=ssh
-    set host (echo $argv[1])
-    set domain (echo $argv[2])
-
-    ssh $host "plesk db -Ne \"SELECT id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
-end
