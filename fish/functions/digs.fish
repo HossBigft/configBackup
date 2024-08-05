53e30172b760c91a5949ea6c46841600fa0d4698
Maxim <m.glushchenko@office.hoster.kz>
2024-08-05 10:10:01 +0500

    Digs: returns ip and success status code, if given ip to recreate undocumented dig behavior

--- fish/functions/digs.fish
+++ fish/functions/digs.fish
@@ -1,6 +1,12 @@
 function digs
     set argsLen (count $argv)
+    set ipRegexPattern "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}"
+    
     if test $argsLen -eq 1 
+        if string match -qr $ipRegexPattern $argv
+            printf $argv
+            return 0
+        end
         set answer (dig +short @8.8.8.8  $argv)
         if test -n "$answer"
             printf %s\n  $answer
