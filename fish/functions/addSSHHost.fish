b3d64ee9e6a4ba9abe34d2561b5ec94f09abd37a
Maxim <m.glushchenko@office.hoster.kz>
2024-08-09 14:24:50 +0500

    addSSHHost: add flags for private and client configs

--- fish/functions/addSSHHost.fish
+++ fish/functions/addSSHHost.fish
@@ -1,7 +1,12 @@
 function addSSHHost
+    set -l options p/private
+    set -l options c/client
+    argparse -n addSSHHost $options -- $argv
+    or return
+    
     # Check if the correct number of arguments is provided
     if test (count $argv) -ne 2
-        echo "Usage: addSSHHost <user@host> <hostname>"
+        echo "Usage: add_ssh_host <user@host> <hostname>"
         return 1
     end
 
@@ -12,9 +17,18 @@ function addSSHHost
     # Extract username and host from the user_host argument
     set username (string split '@' $user_host)[1]
     set host (string split '@' $user_host)[2]
+    
+    if set -q _flag_private
+        printf "Host %s\n\tHostName %s\n\tUser %s\n\tIdentityFile ~/.ssh/id_rsa\n" $host $hostName  $username >> ~/.ssh/conf.d/private
+        echo "Added host $hostName to private config"
+        
+    else if set -q _flag_client
+        printf "Host %s\n\tHostName %s\n\tUser %s\n\tIdentityFile ~/.ssh/id_rsa\n" $host $hostName  $username >> ~/.ssh/clients
+        echo "Added host $hostName to client config"
+        
+    else
+        printf "Host %s\n\tHostName %s\n\tUser %s\n\tIdentityFile ~/.ssh/id_rsa\n" $host $hostName  $username >> ~/.ssh/config
+        echo "Added host $hostName"
+    end
 
-    printf "Host %s\n\tHostName %s\n\tUser %s\n\tIdentityFile ~/.ssh/id_rsa\n" $host $hostName  $username >> ~/.ssh/config
-
-    # Inform the user
-    echo "Added new ssh host configuration for $hostName"
 end
