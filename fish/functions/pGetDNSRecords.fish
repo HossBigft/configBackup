ada6600c3530678c5dc0ee0bad231cb3f7921e0f
Maxim <m.glushchenko@office.hoster.kz>
2024-08-12 09:35:03 +0500

    pGetDNSRecords: add sorting by DNS record type

--- fish/functions/pGetDNSRecords.fish
+++ fish/functions/pGetDNSRecords.fish
@@ -1,3 +1,3 @@
 function pGetDNSRecords --wraps=ssh
- ssh $argv[1] "plesk bin dns --info $argv[2]"|awk NF|head -n -1; 
+ ssh $argv[1] "plesk bin dns --info $argv[2]" | awk NF | head -n -1 | sort -k2; 
 end
