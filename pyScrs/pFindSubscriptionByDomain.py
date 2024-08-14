c612cbcc60212d58e6f2c65b800ac90b37301489
Maxim <m.glushchenko@office.hoster.kz>
2024-08-14 10:15:12 +0500

    pFindSubscriptionByDomain: add username and login output

--- pyScrs/pFindSubscriptionByDomain.py
+++ pyScrs/pFindSubscriptionByDomain.py
@@ -120,21 +120,20 @@ if __name__ == "__main__":
         action="store_true",
         help="print server hostname",
     )
+    parser.add_argument(
+        "-u",
+        "--user",
+        action="store_true",
+        help="print user name and login",
+    )
 
     args = parser.parse_args()
     v = vars(args)
     argsNumber = sum([1 for a in v.values() if a])
     if argsNumber == 1:
-        args.verbose = args.name = args.id = args.domains = args.server = True
-
-    results = ase.batch_ssh_command_result(
-        SERVER_LIST,
-        SSH_USER,
-        f"plesk db -Ne \\\"SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '{args.domainToFind}%';"
-        + f"SELECT name FROM domains WHERE id=(SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '{args.domainToFind}%');"
-        + f"SELECT name FROM domains WHERE webspace_id=(SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '{args.domainToFind}%')\\\"",
-        verbose=args.verbose,
-    )
+        args.verbose = args.name = args.id = args.domains = args.server = args.user = (
+            True
+        )
 
     output_elements = []
     if args.server:
@@ -142,11 +141,6 @@ if __name__ == "__main__":
             output_elements.append("Host:{hostname}")
         else:
             output_elements.append("{hostname}")
-
-    if args.verbose and args.server:
-        print(
-            f"\nSubscription with {args.domainToFind} domain was found on following servers:"
-        )
 
     if args.id:
         if args.verbose:
@@ -160,25 +154,54 @@ if __name__ == "__main__":
         elif args.name:
             output_elements.append("{subscription_name}")
 
+    if args.user:
+        if args.verbose:
+            output_elements[:] = [
+                *output_elements,
+                *["Username:{username}", "Userlogin:{userlogin}"],
+            ]
+        else:
+            output_elements[:] = [*output_elements, *["{username}", "{userlogin}"]]
+
+    results = ase.batch_ssh_command_result(
+        SERVER_LIST,
+        SSH_USER,
+        f"plesk db -Ne \\\"SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '{args.domainToFind}%';"
+        + f"SELECT name FROM domains WHERE id=(SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '{args.domainToFind}%');"
+        + f"SELECT pname, login FROM clients WHERE id=(SELECT cl_id FROM domains WHERE name LIKE '{args.domainToFind}%');"
+        + f"SELECT name FROM domains WHERE webspace_id=(SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '{args.domainToFind}%')\\\"",
+        verbose=args.verbose,
+    )
+
     results = [
         {
             "host": x["host"],
             "id": x["stdout"].strip().split("\n")[0],
             "name": x["stdout"].strip().split("\n")[1],
-            "domains": x["stdout"].strip().split("\n")[2:],
+            "username": x["stdout"].strip().split("\n")[2].split("\t")[0],
+            "userlogin": x["stdout"].strip().split("\n")[2].split("\t")[1],
+            "domains": x["stdout"].strip().split("\n")[4:],
         }
         for x in results
         if x["stdout"]
     ]
+
+    if args.verbose and args.server:
+        print(
+            f"\nSubscription with {args.domainToFind} domain was found on following servers:"
+        )
+
     if results:
         for record in results:
-            if args.name or args.id or args.server:
+            if args.name or args.id or args.server or args.user:
                 print(
                     "|".join(output_elements).format(
                         domaintoFind=args.domainToFind,
                         hostname=record["host"],
                         subscription_id=record["id"],
                         subscription_name=record["name"],
+                        username=record["username"],
+                        userlogin=record["userlogin"],
                     )
                 )
             if args.domains:
