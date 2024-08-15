69ac68e1879fea298023cbb9ee81a6a351ad3643
Maxim <m.glushchenko@office.hoster.kz>
2024-08-15 16:50:00 +0500

    pSetPhpVersion: fix answer when command set fails

--- fish/functions/pSetPhpVersion.fish
+++ fish/functions/pSetPhpVersion.fish
@@ -5,8 +5,9 @@ function pSetPhpVersion --wraps==ssh --wraps=ssh
     set availablePhpRegex "4.4|5.1|5.2|5.3|5.4|5.5|5.6|7.0|7.1|7.2|7.3|7.4|8.0|8.1|8.2|8.3"
     if string match -aqr $phpVersion $availablePhpRegex
         set username (echo $domain|string replace "." "_" )
-        ssh maximg@$host "selectorctl --set-user-current=$phpVersion --user=$username"
-        printf "Set PHP %s for user %s" $phpVersion $username
+        if ssh maximg@$host "selectorctl --set-user-current=$phpVersion --user=$username"
+            printf "Set PHP %s for user %s" $phpVersion $username
+        end
     else
         printf "Available PHP versions: $availablePhpRegex"
     end
