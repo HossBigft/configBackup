3510962b6393ab6c5e854cae7f16b5fc7e457d6b
Maxim <m.glushchenko@office.hoster.kz>
2024-08-15 09:54:26 +0500

    Add abbr pQuery for sending queries to the plesk servers

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 abbr -a -- pFindSubscriptionByName 'python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py'
+abbr -a -- pFindSubscriptionByName 'python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py'
+abbr -a -- pQuery 'python3.12 /home/gmv/configBackup/pyScrs/pQuery.py'
