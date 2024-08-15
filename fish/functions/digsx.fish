71e4582a371c37e2009013021b2b4413832770cf
Maxim <m.glushchenko@office.hoster.kz>
2024-08-15 12:36:19 +0500

    digsx: now can take on list of IPs

--- fish/functions/digsx.fish
+++ fish/functions/digsx.fish
@@ -1,12 +1,17 @@
 function digsx
     if test (count $argv) -eq 0
         return 1
-    end
-    if set answer (digs $argv)
-        digs -x $answer
-        return 0
+    else if test (count $argv) -eq 1
+        if set answer (digs $argv)
+            digs -x $answer
+            return 0
+        else
+            echo "no Result for $ip"
+            return 1
+        end
     else
-        echo no Result for $argv
-        return 1
+        for ip in $argv
+               echo (digs -x $ip)
+        end
     end
 end
