d823af8a5d09b501d77b5f6a492a48dbfbf88f53
Maxim <m.glushchenko@office.hoster.kz>
2024-08-21 09:56:53 +0500

    Add pMailBlacklistDomain: adds domain to mailserver blacklist on specified plesk server

---
+++ fish/functions/pMailBlacklistDomain.fish
@@ -0,0 +1,8 @@
+function pMailBlacklistDomain --wraps=ssh
+    set host (echo $argv[1])
+    set domain (echo $argv[2])
+
+    ssh $host "plesk bin mailserver --add-to-black-list $domain"
+    printf "Domain %s was added to mail server spamlist on %s" $domain $host
+
+end
