6833f44936e6f2a2983bd21018cd4604bb67b865
Maxim <m.glushchenko@office.hoster.kz>
2024-08-26 16:28:17 +0500

    pPhpOptionsGet: finish basic functionality

--- fish/functions/pPhpOptionsGet.fish
+++ fish/functions/pPhpOptionsGet.fish
@@ -1,8 +1,8 @@
 function pPhpOptionsGet --wraps=ssh
     set host (echo $argv[1])
     set domain (echo $argv[2])
-    set username (echo $domain|string replace "." "_")
+    set username (echo $domain|string replace -ra "\.|-" "_")
     
-    ssh p-cloud-4 "selectorctl  --print-options --version=5.3 --user=$username --json"|jello -s|grep "\.value"| string replace "_.data." ""
+    ssh maximg@$host "curPHPVersion=\$(selectorctl --user-summary --user=$username|grep s|cut -d \" \" -f1);selectorctl  --print-options --version=\$curPHPVersion --user=$username --json" | jello -s|grep "\.value"| string replace "_.data." ""
 
 end
