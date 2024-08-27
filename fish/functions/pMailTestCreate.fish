47f9f227e0a649d3a64018e6b9c545642591103e
Maxim <m.glushchenko@office.hoster.kz>
2024-08-27 16:17:03 +0500

    pMailTestCreate: add login link generation

--- fish/functions/pMailTestCreate.fish
+++ fish/functions/pMailTestCreate.fish
@@ -1,8 +1,23 @@
 function pMailTestCreate --wraps=ssh
-    set host (echo $argv[1])
-    set domain (echo $argv[2])
+    set argNum (count $argv)
+
+    if test $argNum -eq 1
+        set domain (echo $argv)
+        set host (_findPleskHost $domain)
+        if test -z "$host"
+            echo "Host for $domain was not found"
+            return 1
+        else
+            echo "Host for $domain is $host"
+        end
+    else if test $argNum -eq 2
+        set domain (echo $argv[2])
+        set host (echo $argv[1])
+    end
+
     set testMail "testhostermail"
     set password (pwgen | string split " " -f1)
     ssh maximg@$host "plesk bin mail --create $testMail@$domain -passwd \"$password\" -mailbox true -description \"$password\""
-    printf $password"\n"
+    echo "Login link:https://webmail.$domain/roundcube/index.php?_user=$testMail%40$domain"
+    echo $password
 end
