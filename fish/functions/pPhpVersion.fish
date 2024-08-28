e02ae44a94a45da8d0b2caa2fc7f5351bd08b2b8
Maxim <m.glushchenko@office.hoster.kz>
2024-08-28 14:15:55 +0500

    pPhpVersion: add host search

--- fish/functions/pPhpVersion.fish
+++ fish/functions/pPhpVersion.fish
@@ -1,7 +1,21 @@
 function pPhpVersion --wraps=ssh
-    set host (echo $argv[1])
-    set domain (echo $argv[2])
-    set username (echo $domain|string replace -ra "\.|-" "_")
+    set argNum (count $argv)
+
+    if test $argNum -eq 1
+        set domain (echo $argv)
+        set username (echo $domain|string replace -ar "\.|-" "_")
+        set host (_findPleskHost $domain)
+        if test $status -eq 1
+            echo "Host for $domain was not found"
+            return 1
+        else
+            echo "Host for $domain is $host"
+        end
+    else if test $argNum -eq 2
+        set domain (echo $argv[2])
+        set username (echo $domain|string replace -ar "\.|-" "_")
+        set host (echo $argv[1])
+    end
 
     ssh maximg@$host "selectorctl --user-summary --user=$username|grep s|cut -d \" \" -f1"
 end
