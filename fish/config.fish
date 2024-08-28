8918e587180727e35557b29c76185c61ff6f6705
Maxim <m.glushchenko@office.hoster.kz>
2024-08-28 14:56:54 +0500

    Add abbr mySshKey: prints content of my ssh key

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-abbr -a -- pFindSubscriptionByName 'python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py'
-abbr -a -- pFindSubscriptionByName 'python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py'
+
 abbr -a -- pQuery 'python3.12 /home/gmv/configBackup/pyScrs/pQuery.py'
+abbr -a -- pFindSubscriptionByName 'python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py'
+abbr -a -- mySshKey 'cat /home/gmv/.ssh/id_rsa.pub'
