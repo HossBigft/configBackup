0a2aaeb1a58b7c53680ed254d87a5eeb59ae2213
Maxim <m.glushchenko@office.hoster.kz>
2024-08-28 09:02:59 +0500

    Add pMailGetTestMailPassword: takes domain name or plesk server host and domain, returns password of test email

---
+++ fish/functions/pMailGetTestMailPassword.fish
@@ -0,0 +1,23 @@
+function pMailGetTestMailPassword
+    set argNum (count $argv)
+
+    if test $argNum -eq 1
+        set domain (echo $argv)
+        set host (_findPleskHost $domain)
+        if test -z "$host"
+            echo "Host for $domain was not found"
+            return 1
+        else
+            echo "Host for $domain is $host"
+        end
+    else if test $argNum -eq 2
+        set domain (echo $argv[2])
+        set host (echo $argv[1])
+    else
+        echo Wrong input
+        return 1
+    end
+    set testMail "testhostermail"
+    echo  testhostermail@$domain password: (ssh maximg@$host  "plesk bin mail --info testhostermail@$domain"|grep Description|string replace -r ' +' ' '|string split " " -f2)
+    return 0
+end
