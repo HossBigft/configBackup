d87f27c1d92539ae462fc33f7d4e8cab58ff9f36
Maxim <m.glushchenko@office.hoster.kz>
2024-08-28 12:30:19 +0500

    pGetDNSRecords: add domain search result check by execution status

--- fish/functions/pGetDNSRecords.fish
+++ fish/functions/pGetDNSRecords.fish
@@ -1,3 +1,23 @@
 function pGetDNSRecords --wraps=ssh
- ssh $argv[1] "plesk bin dns --info $argv[2]" | awk NF | head -n -1 | sort -k2; 
+    set argNum (count $argv)
+    
+    if test $argNum -eq 1
+        set domain (echo $argv)
+        set host (_findPleskHost $domain)
+        if test $status -eq 1
+            echo "Host for $domain was not found"
+            return 1
+        else
+            echo "Host for $domain is $host"
+        end
+    else if test $argNum -eq 2
+        set domain (echo $argv[2])
+        set host (echo $argv[1])
+    else
+        echo Wrong input
+        return 1
+    end
+    
+    ssh maximg@$host "plesk bin dns --info $domain" | awk NF | head -n -1 | sort -k2; 
+    return 0
 end
