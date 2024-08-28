1c865be2efa88669b2bd2118a74321f6ba23591c
Maxim <m.glushchenko@office.hoster.kz>
2024-08-28 12:30:59 +0500

    _findPleskHost: add check for multiple found hosts

--- fish/functions/_findPleskHost.fish
+++ fish/functions/_findPleskHost.fish
@@ -1,9 +1,16 @@
 function _findPleskHost
     set domain (echo $argv[1])
     set host (digsx $domain)
-    if string match -rq "no Result for" $host
-        set host (python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py -s $domain)
-        if string match -rq "No servers was found" $host
+    if string match -rq "no Result for" $host; or not string match -rq "hoster.kz"
+        set host (python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py -s $domain|string collect)
+        
+        if test (echo $host | string split "\n" | count) -gt 1
+            echo Multiple hosts found 
+            echo $host
+            return 1
+        end
+        
+        if string match -rq "No servers was found" $host 
             return 1
         else
             echo $host
