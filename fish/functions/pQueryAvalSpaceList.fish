a926a962466cbbf1f16a48f4be5e4260f9a59ffd
Maxim <m.glushchenko@office.hoster.kz>
2024-08-29 15:24:38 +0500

    pQueryAvalSpaceList: rewrite for async requests

--- fish/functions/pQueryAvalSpaceList.fish
+++ fish/functions/pQueryAvalSpaceList.fish
@@ -3,18 +3,9 @@ function pQueryAvalSpaceList
     set filename pleskAvailableSpace
     set servers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Pv "ns.*hoster.kz|((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
 
-    
-    echo "Starting queries"
-    mkdir -p ~/pkzStats && touch ~/pkzStats/$filename$currDate.txt
-    
-    for server in $servers
-        echo "Querying $server"
-        set response (ssh maximg@$server 'df -BG' | tr -s ' ' | string join "; " | grep -Po "(?:\S+\s+){5}\/var;|((?:\S+\s+){5}\/;)(?!.*\/var;)")
-        echo "$server  answered $response"
-        echo $server $response >>~/pkzStats/$filename$currDate.txt
-    end
+    python3.12 /home/gmv/configBackup/pyScrs/pQuery.py $filename "df -BG | tr -s ' '|sed ':a; N; \\\$!ba; s/\\\n/; /g'|grep -Po '(?:\\\S+\\s+){5}\\\/var;|((?:\\\S+\\\s+){5}\\\/;)(?!.*\\\/var;)'"
     
     echo Sorting
-    sort -t' ' -k6 -r -o ~/pkzStats/$filename$currDate.txt{,}
+    sort -t' ' -k5 -r -o ~/pkzStats/$filename$currDate.txt{,}
     echo "Saved in ~/pkzStats/$filename$currDate.txt"
 end
