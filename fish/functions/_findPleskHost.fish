de1ad7d36382e1bd8a952e4f5881d4733035cffe
Maxim <m.glushchenko@office.hoster.kz>
2024-08-29 09:16:28 +0500

    _findPleskHost: fix typo in hostname check

--- fish/functions/_findPleskHost.fish
+++ fish/functions/_findPleskHost.fish
@@ -1,7 +1,7 @@
 function _findPleskHost
     set domain (echo $argv[1])
     set host (digsx $domain)
-    if string match -rq "no Result for" $host; or not string match -rq "hoster.kz"
+    if string match -rq "no Result for" $host; or not string match -rq "hoster.kz" $host
         set host (python3.12 /home/gmv/configBackup/pyScrs/pFindSubscriptionByDomain.py -s $domain|string collect)
         
         if test (echo $host | string split "\n" | count) -gt 1
