db569b109076e5995b03c3241e97f2a4421f022e
Maxim <m.glushchenko@office.hoster.kz>
2024-08-29 14:04:19 +0500

    nsGetZoneMaster: now accepts list of domain

--- fish/functions/nsGetZoneMaster.fish
+++ fish/functions/nsGetZoneMaster.fish
@@ -1,12 +1,29 @@
 function nsGetZoneMaster
     set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
-    echo Querying Hoster.KZ DNS servers about $argv
-    for host in $dnsServers
-        set zoneMaster (ssh root@$host "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf"| grep \"$argv\" | grep -Po "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
-        echo (echo $host|grep -Po "^([^.])+")\|  $zoneMaster\|
+    set domains (echo $argv | string split " ")
+    set domainCount (count $domains)
+    
+    if test $domainCount -eq 1
+        echo Querying Hoster.KZ DNS servers about $argv
+        for host in $dnsServers
+            set zoneMaster (ssh root@$host "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf"| grep \"$argv\" | grep -Po "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
+            echo (echo $host|grep -Po "^([^.])+")\|  $zoneMaster\|
 
-        if set digAnswer (fish -c "digsx $zoneMaster")
-            echo $digAnswer
+            if set digAnswer (fish -c "digsx $zoneMaster")
+                echo $digAnswer
+            end
+        end
+    else
+        for domain in $domains
+            echo Querying Hoster.KZ DNS servers about $domain
+            for host in $dnsServers
+                set zoneMaster (ssh root@$host "cat /var/opt/isc/scls/isc-bind/zones/_default.nzf"| grep \"$domain\" | grep -Po "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
+                echo (echo $host|grep -Po "^([^.])+")\|  $zoneMaster\|
+
+                if set digAnswer (fish -c "digsx $zoneMaster")
+                    echo $digAnswer
+                end
+            end
         end
     end
 end
