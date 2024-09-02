09b16c409f862fb2baac0b4b313a964320d6e194
Maxim <m.glushchenko@office.hoster.kz>
2024-09-02 09:44:05 +0500

    pCheckSiteBackup: add back size output

--- fish/functions/pCheckSiteBackup.fish
+++ fish/functions/pCheckSiteBackup.fish
@@ -1,5 +1,5 @@
 function pCheckSiteBackup --wraps==ssh --wraps=ssh
     set host (echo $argv[1])
     set domain (echo $argv[2])
-    ssh $host "find  /backup* -maxdepth 6 -type d -name $domain|xargs ls"  
+    ssh $host "find  /backup* -maxdepth 6 -type d -name $domain|xargs du -sh"  
 end
