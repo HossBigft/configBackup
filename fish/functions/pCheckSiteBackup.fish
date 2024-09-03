1b863bf2e9dbf2898e5f1aaf1bac8ea324e68a4a
Maxim <m.glushchenko@office.hoster.kz>
2024-09-03 10:23:46 +0500

    pCheckSiteBackup: add output of backup dirs size in mb

--- fish/functions/pCheckSiteBackup.fish
+++ fish/functions/pCheckSiteBackup.fish
@@ -1,5 +1,5 @@
 function pCheckSiteBackup --wraps==ssh --wraps=ssh
     set host (echo $argv[1])
     set domain (echo $argv[2])
-    ssh $host "find  /backup* -maxdepth 6 -type d -name $domain|xargs du -sh"  
+    ssh $host "find  /backup* -maxdepth 6 -type d -name $domain* | sort | tail -n1 | xargs du -am --max-depth=1 | awk '{printf \"%.3f MiB %s\n\", \$1/1024, \$2}'"  
 end
