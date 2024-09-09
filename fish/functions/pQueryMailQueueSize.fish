58747b0e8592eb6535ca7e836e93c526b39ace39
Maxim <m.glushchenko@office.hoster.kz>
2024-09-09 19:32:34 +0500

    pQueryMailQueueSize: fix problem with different names of files

--- fish/functions/pQueryMailQueueSize.fish
+++ fish/functions/pQueryMailQueueSize.fish
@@ -1,11 +1,12 @@
 function pQueryMailQueueSize
     set  filename "pleskMailQueueSize"
     set  currDate (date +%Y%m%d_%H%M)
-    set filePath ~/pkzStats/$filename$currDate.txt
+
     
     python3.12 /home/gmv/configBackup/pyScrs/pQuery.py $filename "mailq | grep -c '^[A-F0-9]'"
     
     echo Sorting..
-    sort -t'|' -n -k2  --output $filePath{,}
-    echo "Sorted file saved in $filePath"
+    set filePath (ls ~/pkzStats/$filename* | tail -n 1)
+    sort -t'|' -n -k2  --output (ls ~/pkzStats/$filename* | tail -n 1){,}
+    echo "Sorted file saved in $filepath"
 end
