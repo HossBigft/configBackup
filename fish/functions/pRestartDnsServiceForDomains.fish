936ca53a724dea6fa3b97789fc28ed477d3806dd
Maxim <m.glushchenko@office.hoster.kz>
2024-09-13 17:26:30 +0500

    pRestartDnsServiceForDomains.fish: now should acceppt multiple domains as strings

--- fish/functions/pRestartDnsServiceForDomains.fish
+++ fish/functions/pRestartDnsServiceForDomains.fish
@@ -1,6 +1,7 @@
 function pRestartDnsServiceForDomains --wraps==ssh --wraps=ssh
     set host (echo $argv[1])
-    set domains (echo $argv[2])
+    set domains (echo $argv[2]|string split " " -n)
+    echo $domains
     for domain in $domains
         echo "restarting DNS service for $domain"
         ssh $host "plesk bin dns --off $domain && plesk bin dns --on $domain"
