c546c521799e29ca14cfe20bfeabbaee999a81a3
Maxim <m.glushchenko@office.hoster.kz>
2024-09-18 12:32:27 +0500

    _pDomainToCageFsUsername.fish: add reverse function to conver cagefs usernames back to domains

--- fish/functions/_pDomainToCageFsUsername.fish
+++ fish/functions/_pDomainToCageFsUsername.fish
@@ -1,8 +1,14 @@
 function _pDomainToCageFsUsername
+    set -l options r/reverse
+    argparse -n _pDomainToCageFsUsername $options -- $argv
     set argNum (count $argv)
 
-    if test $argNum -eq 1
-        string shorten -m12 -c "" $argv | string replace -ar "\." "_"
+    if test $argNum -eq 2 || test $argNum -eq 1
+        if  set -q _flag_reverse
+         echo $argv | string replace -ar "_" "."
+        else
+            string shorten -m12 -c "" $argv | string replace -ar "\." "_"
+        end
     else
         echo Too much arguments
     end
