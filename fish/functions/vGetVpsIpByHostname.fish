3a076df5a691f6a579e41f52458072b65c6fed47
Maxim <m.glushchenko@office.hoster.kz>
2024-09-19 13:00:55 +0500

    vGetVpsIpByHostname.fish: add check for nonexisting server

--- fish/functions/vGetVpsIpByHostname.fish
+++ fish/functions/vGetVpsIpByHostname.fish
@@ -9,5 +9,11 @@ function vGetVpsIpByHostname
         return 1
     end
     set vpsHostName (echo $argv)
-    curl -s "https://virtualizor.hoster.kz:4085/index.php?act=vs&vpshostname=$vpsHostName&api=json&adminapikey=$VZR_API_KEY&adminapipass=$VZR_API_PASS"|jello  -rl '[ip for server_record in _.vs.values() for ip in server_record.ips.values()]'
+    set requestResult (curl -s "https://virtualizor.hoster.kz:4085/index.php?act=vs&vpshostname=$vpsHostName&api=json&adminapikey=$VZR_API_KEY&adminapipass=$VZR_API_PASS")
+    if not grep -q $vpsHostName (echo $requestResult|psub)
+        echo "Host $vpsHostName was not found"
+        return 1
+    else
+        echo $requestResult | jello  -rl '[ip for server_record in _.vs.values() for ip in server_record.ips.values()]' 
+    end
 end
