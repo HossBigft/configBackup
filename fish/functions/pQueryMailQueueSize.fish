90a7eb18041a6efa7cbefce6cf41c05a91838e84
Maxim <m.glushchenko@office.hoster.kz>
2024-09-19 11:25:32 +0500

    pQueryMailQueueSize.fish: remove variable that doesn't working for whatever reason and fix notification output

--- fish/functions/pQueryMailQueueSize.fish
+++ fish/functions/pQueryMailQueueSize.fish
@@ -2,11 +2,9 @@ function pQueryMailQueueSize
     set  filename "pleskMailQueueSize"
     set  currDate (date +%Y%m%d_%H%M)
 
-    
     python3.12 /home/gmv/configBackup/pyScrs/pQuery.py $filename "mailq | grep -c '^[A-F0-9]'"
     
     echo Sorting..
-    set filePath (ls ~/pkzStats/$filename* | tail -n 1)
     sort -t'|' -n -k2  --output (ls ~/pkzStats/$filename* | tail -n 1){,}
-    echo "Sorted file saved in $filepath"
+    echo "Sorted file saved in "(ls ~/pkzStats/$filename* | tail -n 1)
 end
