66a9ae5efd75041f82849e83a6e571bb63755692
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-09-21 17:57:58 +0500

    pMailTestCreate.fish: merge functionality from pMailCredentialsTestGet.fish and pMailTestRemove.fish

--- fish/functions/pMailTestCreate.fish
+++ fish/functions/pMailTestCreate.fish
@@ -1,5 +1,10 @@
-function pMailTestCreate --wraps=ssh
+function pMailTestCreate --wraps=ssh --description "Creates test mail account for given domain. If it's already created, returns logpass. Can also remove test account with -r/--remove flag"
+    set -l options r/remove
+    argparse -n pMailTestCreate $options -- $argv
+    or return
+
     set argNum (count $argv)
+    set testMail testhostermail
 
     if test $argNum -eq 1
         set domain (echo $argv)
@@ -17,11 +22,32 @@ function pMailTestCreate --wraps=ssh
         echo Wrong input
         return 1
     end
+    if set -q _flag_remove
+        ssh host "plesk bin mail --remove $testMail@$domain"
+        if test $status eq 0
+            echo Email $testMail@$domain is removed
+            return 0
+        else
+            return 1
+        end
+    end
 
-    set testMail "testhostermail"
-    set password (pwgen 20 | string split " " -f1)
-    ssh $host "plesk bin mail --create $testMail@$domain -passwd \"$password\" -mailbox true -description \"$password\""
-    echo "Login link:https://webmail.$domain/roundcube/index.php?_user=$testMail%40$domain"
-    echo $password
-    return 1
+
+    set password (ssh $host  "plesk bin mail --info $testMail@$domain" | grep Description | string replace -r ' +' ' ' | string split " " -f2)
+    if test -n "$password"
+        set password (pwgen 20 | string split " " -f1)
+        ssh $host "plesk bin mail --create $testMail@$domain -passwd \"$password\" -mailbox true -description \"$password\""
+        if test $status -eq 0
+            echo "Login link:https://webmail.$domain/roundcube/index.php?_user=$testMail%40$domain"
+            echo $password
+            return 0
+        else
+            return 1
+        end
+    else
+        echo "Login link:https://webmail.$domain/roundcube/index.php?_user=$testMail%40$domain"
+        echo $password
+        return 0
+    end
+
 end
