0fe4c876f6c6c6e1c08b370c1d5cd511a5547f33
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-09-21 20:49:29 +0500

    nsRmZoneMaster.fish: Add description

--- fish/functions/nsRmZoneMaster.fish
+++ fish/functions/nsRmZoneMaster.fish
@@ -1,7 +1,7 @@
-function nsRmZoneMaster
+function nsRmZoneMaster --description "Removes zone for given domain from DNS slave servers"
 set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
     for host in $dnsServers
         echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
-        ssh root@$host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
+        ssh $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
     end
 end
