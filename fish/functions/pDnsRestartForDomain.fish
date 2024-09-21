08b66c1bdbcf7795460422b420c87efea00216ca
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-09-21 09:30:02 +0500

    Rename pRestartDnsServiceForDomains.fish -> pDnsRestartForDomain.fish

--- fish/functions/pDnsRestartForDomain.fish
+++ fish/functions/pDnsRestartForDomain.fish
@@ -1,4 +1,4 @@
-function pRestartDnsServiceForDomains --wraps==ssh --wraps=ssh
+function pDnsRestartForDomain  --wraps=ssh --description "Restarts DNS service in Plesk for given domain. Takes server hostname and list of domains. Example pDnsRestartForDomain p-cloud-4 gruzo.kz"
     set host (echo $argv[1])
     set domains (echo $argv[2]|string split " " -n)
     echo $domains
