b091507a24f2899c48cf552ce35e2cf1cfa266bb
Maxim <m.glushchenko@office.hoster.kz>
2024-09-23 12:02:18 +0500

    pGetSubscriptionIDByDomain.fish: update functionality and add aility to search host

--- fish/functions/pGetSubscriptionIDByDomain.fish
+++ fish/functions/pGetSubscriptionIDByDomain.fish
@@ -1,15 +1,28 @@
 function pGetSubscriptionIDByDomain --wraps=ssh
-    set argNum (count $argv)
-    
-    if test $argNum -eq 1
-        set domain (echo $argv)
-        set host (digsx $domain|string sub -e-1)
-        ssh maximg@$host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
-        
-    else if test $argNum -eq 2
-        set host (echo $argv[1])
-        set domain (echo $argv[2])
+    set -l options q/quiet
+    argparse -n pGetSubscriptionIDByDomain $options -- $argv
+    or return
 
-        ssh $host "plesk db -Ne \"SELECT webspace_id FROM domains WHERE name LIKE '%$domain%'\"&& plesk db -Ne \"SELECT id FROM domains WHERE name LIKE '%$domain%'\""|grep -Pv "^0\$"| sort -u
+    set argNum (count $argv)
+
+    if test $argNum -eq 1 
+        set domain (echo $argv)
+        set host (_findPleskHost $domain)
+        if not set -q _flag_quiet
+            if test -z "$host"
+                echo "Host for $domain was not found"
+                return 1
+            else
+                echo "Host for $domain is $host"
+            end
+        end
+    else if test $argNum -eq 2 
+        set domain (echo $argv[2])
+        set host (echo $argv[1])
+    else
+        echo Wrong input
+        return 1
     end
+
+        ssh $host "plesk db -Ne \"SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '$domain%'\""
 end
