78fba47dd8f90c4425d65d565259344f1dc656f7
Maxim <m.glushchenko@office.hoster.kz>
2024-09-23 11:54:26 +0500

    pSubscriptionNameByDomain.fish: return from legacy functions and update functionality

--- fish/LEGACY_functions/LEGACY_pGetSubscriptionNameByDomain.fish
+++
@@ -1,7 +0,0 @@
-function pGetSubscriptionNameByDomain --wraps=ssh
-    set host (echo $argv[1])
-    set domain (echo $argv[2])
-    set subscriptionID (pGetSubscriptionIDByDomain $host $domain)
-    pGetSubscriptionNameByID $host $subscriptionID
-
-end
---
+++ fish/functions/pSubscriptionNameByDomain.fish
@@ -0,0 +1,29 @@
+function pSubscriptionNameByDomain --wraps=ssh
+    set -l options q/quiet
+    argparse -n pMailTestCreate $options -- $argv
+    or return
+
+    set argNum (count $argv)
+
+    if test $argNum -eq 1 
+        set domain (echo $argv)
+        set host (_findPleskHost $domain)
+        if not set -q _flag_quiet
+            if test -z "$host"
+                echo "Host for $domain was not found"
+                return 1
+            else
+                echo "Host for $domain is $host"
+            end
+        end
+    else if test $argNum -eq 2 
+        set domain (echo $argv[2])
+        set host (echo $argv[1])
+    else
+        echo Wrong input
+        return 1
+    end
+
+    ssh $host "plesk db -Ne \"SELECT name FROM domains WHERE id=(SELECT CASE WHEN webspace_id = 0 THEN id ELSE webspace_id END AS result FROM domains WHERE name LIKE '$domain%');\""
+
+end
