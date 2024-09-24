297853df8cde3333f62672171c8254d81720d108
Maxim <m.glushchenko@office.hoster.kz>
2024-09-24 14:49:57 +0500

    Rename pCheckEmailSSL.fish -> pSslEmailCheck.fish

--- fish/functions/pSslEmailCheck.fish
+++ fish/functions/pSslEmailCheck.fish
@@ -1,3 +1,3 @@
-function pCheckEmailSSL
+function pSslEmailCheck --description "Returns data of certificate, which used for mail 465 connection"
  openssl s_client -showcerts -connect mail.$argv:465 -servername mail.$argv | openssl x509 -dates -noout; 
 end
