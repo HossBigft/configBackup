dd8a6a2767fa64e5f1afd4cf4c71140b632c69aa
Maxim <m.glushchenko@office.hoster.kz>
2024-09-25 14:11:53 +0500

    findPleskHostByDomain.py: add search on plesk servers

--- pyScrs/findPleskHostByDomain.py
+++ pyScrs/findPleskHostByDomain.py
@@ -2,6 +2,7 @@ import argparse
 from dns import resolver, reversename
 import sys
 import nsZoneMaster
+import pFindSubscriptionByDomain as pfind
 
 
 def main():
@@ -16,7 +17,7 @@ def main():
     )
     args = parser.parse_args()
     domain = args.domain
-    print(f"Starting host resolution for {domain} using A record")
+    print(f"Starting host resolution for {domain} using A record.")
     try:
         a_record = "".join([ipval.to_text() for ipval in resolver.resolve(domain, "A")])
         try:
@@ -26,13 +27,13 @@ def main():
                 print(ptr_record, end="")
                 sys.exit(0)
             else:
-                print(f"No substring 'hoster.kz' in {ptr_record}")
+                print(f"No substring 'hoster.kz' in {ptr_record}.")
         except (resolver.NoAnswer, resolver.NXDOMAIN):
             print(f"[FAIL] PTR record not found for {a_record}. PTR lookup failed.")
     except (resolver.NoAnswer, resolver.NXDOMAIN):
-        print(f"[FAIL] A record not found for {domain}")
+        print(f"[FAIL] A record not found for {domain}.")
 
-    print(f"Attempting to resolve host for {domain} using MX record")
+    print(f"Attempting to resolve host for {domain} using MX record.")
     try:
         mx_record = "".join(
             [ipval.to_text() for ipval in resolver.resolve(domain, "MX")]
@@ -47,14 +48,16 @@ def main():
                 print(ptr_record, end="")
                 sys.exit(0)
             else:
-                print(f"No substring 'hoster.kz' in {ptr_record}")
+                print(f"No substring 'hoster.kz' in {ptr_record}.")
         except (resolver.NoAnswer, resolver.NXDOMAIN):
             print(f"[FAIL] PTR record not found for {mx_record}. PTR lookup failed.")
     except (resolver.NoAnswer, resolver.NXDOMAIN):
-        print(f"[FAIL] A record not found for {mx_record}")
+        print(f"[FAIL] MX record not found for {domain}.")
 
-    print(f"Attempting to resolve host for {domain} using DNS zone master")
-    if zone_master_a_record := "".join(nsZoneMaster.getDomainZoneMaster(domain, verbosity_flag=False)):
+    print(f"Attempting to resolve host for {domain} using DNS zone master.")
+    if zone_master_a_record := "".join(
+        nsZoneMaster.getDomainZoneMaster(domain, verbosity_flag=False)
+    ):
         try:
             addr_record = reversename.from_address(zone_master_a_record)
             ptr_record = str(resolver.resolve(addr_record, "PTR")[0])
@@ -62,23 +65,17 @@ def main():
                 print(ptr_record, end="")
                 sys.exit(0)
             else:
-                print(f"No substring 'hoster.kz' in {ptr_record}")
+                print(f"No substring 'hoster.kz' in {ptr_record}.")
         except (resolver.NoAnswer, resolver.NXDOMAIN):
             print(f"[FAIL] PTR record not found for {a_record}. PTR lookup failed.")
     else:
-        print(f"[FAIL] Zone master not found for {domain}")
+        print(f"[FAIL] Zone master not found for {domain}.")
 
-    # try:
-    #     command = shlex.split(f'python3 pFindSubscriptionByDomain.py -s {domain}"')
-
-    #     output = subprocess.run(command, capture_output=True, text=True, check=True)
-    #     host = output.stdout
-    #     print (host)
-    #     sys.exit(0)
-    # except Exception as e:
-    #     print(f"No servers was found with domain {domain}")
-    #     sys.exit(1)
-
-
+    print(f"Attempting to find host {domain} by querying servers.")
+    if answers:= pfind.query_domain_info(domain, verbose_flag=False):
+        for answer in answers:
+            print(answer["host"])
+    else:
+        print("[ERROR] Host for {domain} not found.")
 if __name__ == "__main__":
     main()
