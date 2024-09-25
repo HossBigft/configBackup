d6a017fe834d7d80d84e7bbd944ec234eb446093
Maxim <m.glushchenko@office.hoster.kz>
2024-09-25 14:42:09 +0500

    findPleskHostByDomain.py: add argument validation and verbose flag

--- pyScrs/findPleskHostByDomain.py
+++ pyScrs/findPleskHostByDomain.py
@@ -3,7 +3,19 @@ from dns import resolver, reversename
 import sys
 import nsZoneMaster
 import pFindSubscriptionByDomain as pfind
+import re
 
+DNS_HOSTING_HOSTNAME = "dns.hoster.kz."
+
+def regex_type(pattern: str | re.Pattern):
+    """Argument type for matching a regex pattern."""
+
+    def closure_check_regex(arg_value):  
+        if not re.match(pattern, arg_value):
+            raise argparse.ArgumentTypeError(f"Invalid domain name: '{arg_value}' is not a valid domain name")
+        return arg_value
+
+    return closure_check_regex
 
 def main():
     parser = argparse.ArgumentParser(
@@ -12,28 +24,40 @@ def main():
 
     parser.add_argument(
         "domain",
-        type=str,
-        help="Domain name of the site for search",
+        type=regex_type(r"(?:(?!-|[^.]+_)[A-Za-z0-9-_]{1,63}(?<!-)(?:\.|$)){2,}"),
+        help="domain name of the site to search",
+    )
+    
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="enables detailed output in the console",
     )
     args = parser.parse_args()
     domain = args.domain
-    print(f"Starting host resolution for {domain} using A record.")
+    verbose_flag=args.verbose
+    if verbose_flag:
+        print(f"Starting host resolution for {domain} using A record.")
+    
     try:
         a_record = "".join([ipval.to_text() for ipval in resolver.resolve(domain, "A")])
         try:
             addr_record = reversename.from_address(a_record)
             ptr_record = str(resolver.resolve(addr_record, "PTR")[0])
-            if "hoster.kz" in ptr_record:
+            if "hoster.kz" in ptr_record and not ptr_record == DNS_HOSTING_HOSTNAME:
                 print(ptr_record, end="")
                 sys.exit(0)
-            else:
+            elif verbose_flag:
                 print(f"No substring 'hoster.kz' in {ptr_record}.")
         except (resolver.NoAnswer, resolver.NXDOMAIN):
-            print(f"[FAIL] PTR record not found for {a_record}. PTR lookup failed.")
+            if verbose_flag:
+                print(f"[FAIL] PTR record not found for {a_record}. PTR lookup failed.")
     except (resolver.NoAnswer, resolver.NXDOMAIN):
-        print(f"[FAIL] A record not found for {domain}.")
-
-    print(f"Attempting to resolve host for {domain} using MX record.")
+        if verbose_flag:
+            print(f"[FAIL] A record not found for {domain}.")
+    if verbose_flag:
+        print(f"Attempting to resolve host for {domain} using MX record.")
     try:
         mx_record = "".join(
             [ipval.to_text() for ipval in resolver.resolve(domain, "MX")]
@@ -44,38 +68,53 @@ def main():
         try:
             addr_record = reversename.from_address(a_record)
             ptr_record = str(resolver.resolve(addr_record, "PTR")[0])
-            if "hoster.kz" in ptr_record:
+            if "hoster.kz" in ptr_record and not ptr_record == DNS_HOSTING_HOSTNAME:
                 print(ptr_record, end="")
                 sys.exit(0)
             else:
-                print(f"No substring 'hoster.kz' in {ptr_record}.")
+                if verbose_flag:
+                    print(f"No substring 'hoster.kz' in {ptr_record}.")
         except (resolver.NoAnswer, resolver.NXDOMAIN):
-            print(f"[FAIL] PTR record not found for {mx_record}. PTR lookup failed.")
+            if verbose_flag:
+                print(f"[FAIL] PTR record not found for {mx_record}. PTR lookup failed.")
     except (resolver.NoAnswer, resolver.NXDOMAIN):
-        print(f"[FAIL] MX record not found for {domain}.")
-
-    print(f"Attempting to resolve host for {domain} using DNS zone master.")
+        if verbose_flag:
+            print(f"[FAIL] MX record not found for {domain}.")
+    if verbose_flag:
+        print(f"Attempting to resolve host for {domain} using DNS zone master.")
     if zone_master_a_record := "".join(
         nsZoneMaster.getDomainZoneMaster(domain, verbosity_flag=False)
     ):
         try:
             addr_record = reversename.from_address(zone_master_a_record)
             ptr_record = str(resolver.resolve(addr_record, "PTR")[0])
-            if "hoster.kz" in ptr_record:
+            if "hoster.kz" in ptr_record and not ptr_record == DNS_HOSTING_HOSTNAME:
                 print(ptr_record, end="")
                 sys.exit(0)
             else:
-                print(f"No substring 'hoster.kz' in {ptr_record}.")
+                if verbose_flag:
+                    print(f"No substring 'hoster.kz' in {ptr_record}.")
         except (resolver.NoAnswer, resolver.NXDOMAIN):
-            print(f"[FAIL] PTR record not found for {a_record}. PTR lookup failed.")
+            if verbose_flag:
+                print(f"[FAIL] PTR record not found for {a_record}. PTR lookup failed.")
     else:
-        print(f"[FAIL] Zone master not found for {domain}.")
-
-    print(f"Attempting to find host {domain} by querying servers.")
-    if answers:= pfind.query_domain_info(domain, verbose_flag=False):
+        if verbose_flag:
+            print(f"[FAIL] Zone master not found for {domain}.")
+    if verbose_flag:
+        print(f"Attempting to find host {domain} by querying servers.")
+    if answers := [
+        answer
+        for answer in pfind.query_domain_info(domain, verbose_flag=False)
+        if not answer["host"] == DNS_HOSTING_HOSTNAME
+    ]:
         for answer in answers:
             print(answer["host"])
+        sys.exit(0)
     else:
-        print("[ERROR] Host for {domain} not found.")
+        if verbose_flag:
+            print("[ERROR] Host for {domain} not found.")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     main()
