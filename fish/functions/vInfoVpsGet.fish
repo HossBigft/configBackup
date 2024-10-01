c7de33b0ad9c04bebbd5b6175062b3725065737c
Maxim <m.glushchenko@office.hoster.kz>
2024-10-01 17:32:11 +0500

    vInfoVpsGet.fish: add disk info parsing

--- fish/functions/vInfoVpsGet.fish
+++ fish/functions/vInfoVpsGet.fish
@@ -1,5 +1,6 @@
-function vInfoVpsGet  --description "Returns info about given Virtualizor VPS by hostname"
-        set argNum (count $argv)
+function vInfoVpsGet  --description "Returns info about given Virtualizor VPS by hostname"    
+    set pythonScriptsPath (find ~ -maxdepth 1 -name configBackup  | head -n1)
+    set argNum (count $argv)
     if test $argNum -gt 1
         echo Too many arguments [$argnum]
         return 1
@@ -14,23 +15,27 @@ function vInfoVpsGet  --description "Returns info about given Virtualizor VPS by
         echo "Host $vpsHostName was not found"
         return 1
     else
-        set parsedResult (echo $requestResult | jello -lr '[f"{k}:{v}" for server_record in _.vs.values() for k,v in server_record.iteritems() if k in ["hostname", "vps_name", "os_name", "space", "ram", "cores", "network_speed", "upload_speed", "suspended", "nic_type", "cpu_mode", "server_name", "email"] or (k == "ips" and {f"ips:{ip}" for ip in v.values()})]')
+        set parsedResult (echo $requestResult | jello -lr '[f"{k}:{v}" for server_record in _.vs.values() for k,v in server_record.iteritems() if k in ["hostname", "vps_name", "vpsid", "os_name", "space", "ram", "cores", "network_speed", "upload_speed", "suspended", "nic_type", "cpu_mode", "server_name", "email", "cached_disk"] or (k == "ips" and {f"ips:{ip}" for ip in v.values()})]')
         set vpsId (string split ":" -f2 $parsedResult[1])
-        set vpsHostname (string split ":" -f2 $parsedResult[2])
-        set os_name (string split ":" -f2 $parsedResult[3])
-        set space (string split ":" -f2 $parsedResult[4])
-        set ram (string split ":" -f2 $parsedResult[5])
-        set cores (string split ":" -f2 $parsedResult[6])
-        set network_speed (string split ":" -f2 $parsedResult[7])
-        set upload_speed (string split ":" -f2 $parsedResult[8])
-        set suspended (string split ":" -f2 $parsedResult[9])
-        set nic_type (string split ":" -f2 $parsedResult[10])
-        set cpu_mode (string split ":" -f2 $parsedResult[11])
-        set server_name (string split ":" -f2 $parsedResult[12])
-        set email (string split ":" -f2 $parsedResult[13])
-        set ip (string split ":" -f2 $parsedResult[14]|grep -Po "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
+        set vpsVId (string split ":" -f2 $parsedResult[2])
+        set vpsHostname (string split ":" -f2 $parsedResult[3])
+        set os_name (string split ":" -f2 $parsedResult[4])
+        set space (string split ":" -f2 $parsedResult[5])
+        set ram (string split ":" -f2 $parsedResult[6])
+        set cores (string split ":" -f2 $parsedResult[7])
+        set network_speed (string split ":" -f2 $parsedResult[8])
+        set upload_speed (string split ":" -f2 $parsedResult[9])
+        set suspended (string split ":" -f2 $parsedResult[10])
+        set nic_type (string split ":" -f2 $parsedResult[11])
+        set cpu_mode (string split ":" -f2 $parsedResult[12])
+        set diskInfo (string split ":" -f2 -m1 $parsedResult[13])
+        set server_name (string split ":" -f2 $parsedResult[14])
+        set email (string split ":" -f2 $parsedResult[15])
+        set ip (string split ":" -f2 $parsedResult[16]|grep -Po "((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}")
+
+        
         echo IP\|$ip
-        echo ID\|$vpsId
+        echo ID\\VID\|$vpsId\\$vpsVId
         echo Hostname\|$vpsHostname
         echo User\|$email
         echo Is suspended\|$suspended
