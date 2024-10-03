3656e4f0fa2c444589873e9dca0a5b3201d0ca89
Maxim <m.glushchenko@office.hoster.kz>
2024-10-03 17:35:13 +0500

    pPhpDebugToggle.fish: rewrite with new  _pDomainToCageFsUsername.fish

--- fish/functions/pPhpDebugToggle.fish
+++ fish/functions/pPhpDebugToggle.fish
@@ -1,32 +1,37 @@
 function pPhpDebugToggle --wraps=ssh
-    set -l options n/name
+    set -l options l/literal
     argparse -n pPhpDebugToggle $options -- $argv
     or return
     
     set argNum (count $argv)
     if test $argNum -eq 1
         set domain (echo $argv)
-        if set -q _flag_name
-            set username (echo $domain)
+        set subscriptionName (pSubscriptionNameByDomain $domain -q)
+        set host (_findPleskHost $domain)
+        set username (_pDomainToCageFsUsername $host $subscriptionName)
+        
+        if test $status -ne 0
+            echo "[ERROR] CageFS user for subscription $subscriptionName was not found."
         else
-            set username (_pDomainToCageFsUsername $domain)
+            echo "Found CageFS user $username for subscription $subscriptionName."
         end
-        set host (_findPleskHost (_pDomainToCageFsUsername -r $domain))
-        if test -z "$host"
-            echo "Host for $domain was not found"
+        
+        if test $status -eq 1
+            echo "[ERROR] Host for $domain was not found"
             return 1
         else
             echo "Host for $domain is $host"
         end
     else if test $argNum -eq 2
+        set subscriptionName (echo $argv[2])
         set host (echo $argv[1])
-        set domain (echo $argv[2])
-        if set -q _flag_name
+        
+        if set -q _flag_literal
             set username (echo $domain)
-        else
-            set username (_pDomainToCageFsUsername $domain)
+        else    
+            set username (_pDomainToCageFsUsername $host $subscriptionName)
         end
     end
-        ssh maximg@$host "curPHPVersion=\$(selectorctl --user-summary --user=$username|grep s|cut -d \" \" -f1); displayErrorsValue=\$(selectorctl  --print-options --version=\$curPHPVersion --user=$username --csv|grep -Po \"display_errors.*\"on\"\"); if [[ \$displayErrorsValue ]]; then selectorctl --replace-options=display_errors:off --version=\$curPHPVersion --user=$username && echo \"display_errors:off for user $username PHP \$curPHPVersion\"; else selectorctl --replace-options=display_errors:on --version=\$curPHPVersion --user=$username && echo \"display_errors:on for user $username PHP \$curPHPVersion\"; fi"
+        ssh $host "curPHPVersion=\$(selectorctl --user-summary --user=$username|grep s|cut -d \" \" -f1); displayErrorsValue=\$(selectorctl  --print-options --version=\$curPHPVersion --user=$username --csv|grep -Po \"display_errors.*\"on\"\"); if [[ \$displayErrorsValue ]]; then selectorctl --replace-options=display_errors:off --version=\$curPHPVersion --user=$username && echo \"display_errors:off for user $username PHP \$curPHPVersion\"; else selectorctl --replace-options=display_errors:on --version=\$curPHPVersion --user=$username && echo \"display_errors:on for user $username PHP \$curPHPVersion\"; fi"
         return 0
 end
