392d2a3760d4fa23ebff5503fb43bc9eb4834268
Maxim <m.glushchenko@office.hoster.kz>
2024-10-03 17:31:36 +0500

    pPhpCageFsUserRestart.fish: rewrite with new  _pDomainToCageFsUsername.fish

--- fish/functions/pPhpCageFsUserRestart.fish
+++ fish/functions/pPhpCageFsUserRestart.fish
@@ -1,20 +1,28 @@
 function pPhpCageFsUserRestart --wraps=ssh --description 'Restarts CageFS user for given subscription. Takes server hostname, name of subscription or can take only name of subscription and try to find host'
- set argNum (count $argv)
+    set argNum (count $argv)
 
     if test $argNum -eq 1
         set domain (echo $argv)
-        set username (_pDomainToCageFsUsername $domain)
+        set subscriptionName (pSubscriptionNameByDomain $domain -q)
         set host (_findPleskHost $domain)
+        set username (_pDomainToCageFsUsername $host $domain)
+        
+        if test $status -ne 0
+            echo "[ERROR] CageFS user for subscription $subscriptionName was not found."
+        else
+            echo "Found CageFS user $username for subscription $subscriptionName."
+        end
+        
         if test $status -eq 1
-            echo "Host for $domain was not found"
+            echo "[ERROR] Host for $domain was not found"
             return 1
         else
             echo "Host for $domain is $host"
         end
     else if test $argNum -eq 2
-        set domain (echo $argv[2])
-        set username (_pDomainToCageFsUsername $domain)
+        set subscriptionName (echo $argv[2])
         set host (echo $argv[1])
+        set username (_pDomainToCageFsUsername $host $subscriptionName)
     end
     ssh $host "echo Initial status: User $username is \$(cagefsctl --user-status $username)&& cagefsctl --disable $username && echo User $username is \$(cagefsctl --user-status $username) && cagefsctl --enable $username && echo User $username is \$(cagefsctl --user-status $username)"
 end
