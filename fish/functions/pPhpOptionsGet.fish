731399a29e8a1cbb716bd7738bc0964db0ead514
Maxim <m.glushchenko@office.hoster.kz>
2024-10-03 17:44:21 +0500

    pPhpOptionsGet.fish: rewrite with new  _pDomainToCageFsUsername.fish

--- fish/functions/pPhpOptionsGet.fish
+++ fish/functions/pPhpOptionsGet.fish
@@ -1,23 +1,31 @@
-function pPhpOptionsGet --wraps=ssh
+function pPhpOptionsGet --wraps=ssh  --description "Returns list of custom PHP options set in PHP selector, such as debug"
 
     set argNum (count $argv)
 
     if test $argNum -eq 1
         set domain (echo $argv)
-        set username (_pDomainToCageFsUsername $domain)
+        set subscriptionName (pSubscriptionNameByDomain $domain -q)
         set host (_findPleskHost $domain)
-        if test -z "$host"
-            echo "Host for $domain was not found"
+        set username (_pDomainToCageFsUsername $host $subscriptionName)
+        
+        if test $status -ne 0
+            echo "[ERROR] CageFS user for subscription $subscriptionName was not found."
+        else
+            echo "Found CageFS user $username for subscription $subscriptionName."
+        end
+        
+        if test $status -eq 1
+            echo "[ERROR] Host for $domain was not found"
             return 1
         else
             echo "Host for $domain is $host"
         end
     else if test $argNum -eq 2
-        set domain (echo $argv[2])
-        set username (_pDomainToCageFsUsername $domain)
+        set subscriptionName (echo $argv[2])
         set host (echo $argv[1])
+        set username (_pDomainToCageFsUsername $host $subscriptionName)
     end
     
-    ssh maximg@$host "curPHPVersion=\$(selectorctl --user-summary --user=$username|grep s|cut -d \" \" -f1);selectorctl  --print-options --version=\$curPHPVersion --user=$username --json" | jello -s|grep "\.value"| string replace "_.data." ""
+    ssh $host "curPHPVersion=\$(selectorctl --user-summary --user=$username|grep s|cut -d \" \" -f1);selectorctl  --print-options --version=\$curPHPVersion --user=$username --json" | jello -s|grep "\.value"| string replace "_.data." ""
 
 end
