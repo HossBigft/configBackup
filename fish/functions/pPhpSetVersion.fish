2367002eb1d2f5d0424e540cf3b47332aa13a7ef
Maxim <m.glushchenko@office.hoster.kz>
2024-10-04 10:56:39 +0500

    pPhpSetVersion.fish: add validation

--- fish/functions/pPhpSetVersion.fish
+++ fish/functions/pPhpSetVersion.fish
@@ -1,15 +1,77 @@
 function pPhpSetVersion --wraps=ssh
-    set host (echo $argv[1])
-    set domain (echo $argv[2])
-    set phpVersion (echo $argv[3])
+    set argNum (count $argv)
     set availablePhpRegex "4.4|5.1|5.2|5.3|5.4|5.5|5.6|7.0|7.1|7.2|7.3|7.4|8.0|8.1|8.2|8.3"
-    if string match -aqr $phpVersion $availablePhpRegex
-        set username (_pDomainToCageFsUsername $domain)
-        if ssh $host "selectorctl --set-user-current=$phpVersion --user=$username"
-            printf "Set PHP %s for user %s\n" $phpVersion $username
+    if test $argNum -eq 2
+        set domain (echo $argv[1])
+        set phpVersion (echo $argv[2])
+        if string match -aqr $phpVersion $availablePhpRegex
+        
+            set host (_findPleskHost $domain)
+            if test $status -eq 0
+                echo "Host for $domain is $host"
+            else
+                echo "[ERROR] Host for $domain was not found."
+                return 1
+            end
+            
+            set subscriptionName (pSubscriptionNameByDomain $host $domain -q)
+            if test $status -ne 0
+                echo "[ERROR] Subscription $subscriptionName was not found on $host"
+                return 1
+            else
+                echo "Found subscription $subscriptionName on $host."
+            end
+            
+            set username (_pDomainToCageFsUsername $host $subscriptionName)
+            if test $status -ne 0
+                echo "[ERROR] CageFS user for subscription $subscriptionName was not found."
+                return 1
+            else
+                echo "Found CageFS user $username for subscription $subscriptionName"
+            end
+            
+            
+            ssh $host "selectorctl --set-user-current=$phpVersion --user=$username"
+            if test $status -eq 0
+                printf "Set PHP %s for user %s.\n" $phpVersion $username
+            end
+            return $status
+        else
+            printf "[ERROR] PHP $phpVersion not available.\nAvailable PHP versions: $availablePhpRegex\n"
+            return 1
+        end
+    else if test $argNum -eq 3
+        set host (echo $argv[1])
+        
+        
+        if not _isExistingPleskHost $host
+            echo "[ERROR] Host $host does not exist."
+            return 1
+        end
+        
+        set phpVersion (echo $argv[3])
+        
+        if string match -aqr $phpVersion $availablePhpRegex
+            set subscriptionName (echo $argv[2])
+            set username (_pDomainToCageFsUsername $host $subscriptionName)
+            if test $status -ne 0
+                echo "[ERROR] CageFS user for subscription $subscriptionName was not found."
+                return 1
+            else
+                echo "Found CageFS user $username for subscription $subscriptionName"
+            end
+            
+            ssh $host "selectorctl --set-user-current=$phpVersion --user=$username"
+            
+            if test $status -eq 0
+                printf "Set PHP %s for user %s\n" $phpVersion $username
+            end
+        else
+            printf "[ERROR] PHP $phpVersion not available.\nAvailable PHP versions: $availablePhpRegex\n"
+            return 1
         end
     else
-        printf "Available PHP versions: $availablePhpRegex"
+        echo "[ERROR] wrong number of arguments[$argNum]"
     end
 
 end
