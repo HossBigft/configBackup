6702e9adbf9c21c651f41eeae9490d4bf259cadd
Maxim <m.glushchenko@office.hoster.kz>
2024-10-07 10:14:25 +0500

    Rename findPleskHostByDomain.py -> pFindPleskHostByDomain.py

--- fish/functions/_findPleskHost.fish
+++ fish/functions/_findPleskHost.fish
@@ -1,4 +1,4 @@
 function _findPleskHost --description "Tries to find Plesk server hostname by given domain"
     set scriptsPath (find ~ -maxdepth 1 -name configBackup  | head -n1)
-    python3 $scriptsPath/pyScrs/findPleskHostByDomain.py $argv
+    python3 $scriptsPath/pyScrs/pFindPleskHostByDomain.py $argv
 end
