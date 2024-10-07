2084693e9b291cc28b7c0ea88eda733b940c5c4a
Maxim <m.glushchenko@office.hoster.kz>
2024-10-07 17:46:09 +0500

    dns_resolver.py: add types

--- pyScrs/dns_resolver.py
+++ pyScrs/dns_resolver.py
@@ -1,17 +1,18 @@
 from dns import resolver, reversename
+from typing import List
 
 class RecordNotFoundError(Exception):
     def __init__(self, message):
         super().__init__(message)
 
-def resolve_a_record(domain):
+def resolve_a_record(domain:str) -> List[str]:
     try:
         return [ipval.to_text() for ipval in resolver.resolve(domain, "A")]
     except (resolver.NoAnswer, resolver.NXDOMAIN, resolver.NoNameservers) as exc:
         raise RecordNotFoundError(f"A record not found for {domain}") from exc
 
 
-def resolve_ptr_record(ip):
+def resolve_ptr_record(ip:str) -> str:
     try:
         addr_record = reversename.from_address(ip)
         return str(resolver.resolve(addr_record, "PTR")[0])
@@ -19,7 +20,7 @@ def resolve_ptr_record(ip):
         raise RecordNotFoundError(f"PTR record not found for {ip}") from exc
 
 
-def resolve_mx_record(domain):
+def resolve_mx_record(domain) -> List[str]:
     try:
         return "".join(
             [ipval.to_text() for ipval in resolver.resolve(domain, "MX")]
