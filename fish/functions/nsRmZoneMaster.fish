d2bb6f72caddc979860c363369e0025559e25c0b
Maxim <m.glushchenko@office.hoster.kz>
2024-10-11 16:10:55 +0500

    nsRmZoneMaster.fish: add zone master output

--- fish/functions/nsRmZoneMaster.fish
+++ fish/functions/nsRmZoneMaster.fish
@@ -1,7 +1,11 @@
 function nsRmZoneMaster --description "Removes zone for given domain from DNS slave servers"
-set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
+
+    set domain (echo $argv)
+    nsZoneMaster  $domain
+    set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
     for host in $dnsServers
-        echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
-        ssh $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $argv"
+        echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $domain"
+        ssh $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $domain"
     end
+    nsZoneMaster  $domain
 end
