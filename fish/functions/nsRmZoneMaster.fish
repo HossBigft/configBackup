fee34726844247d9f29a30528850c64c53f3d759
Maxim <m.glushchenko@office.hoster.kz>
2024-10-14 14:34:04 +0500

    nsRmZoneMaster.fish: make it accepts list of domains

--- fish/functions/nsRmZoneMaster.fish
+++ fish/functions/nsRmZoneMaster.fish
@@ -1,11 +1,13 @@
 function nsRmZoneMaster --description "Removes zone for given domain from DNS slave servers"
 
-    set domain (echo $argv)
-    nsZoneMaster  $domain
+    set domains (echo $argv)
+    nsZoneMaster   (echo $domains)
     set dnsServers (cat ~/.ssh/config|grep -Po "(?<=Hostname ).*|(?<=HostName ).*"|grep -Po "ns.*")
-    for host in $dnsServers
-        echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $domain"
-        ssh $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $domain"
+    for domain in $domains
+        for host in $dnsServers
+            echo $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $domain"
+            ssh $host "/opt/isc/isc-bind/root/usr/sbin/rndc delzone -clean $domain"
+        end
     end
-    nsZoneMaster  $domain
+    nsZoneMaster (echo $domains)
 end
