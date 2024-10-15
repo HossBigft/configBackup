7bace2bb3d919b63bc4f45a749b5bb7f4fcfbcf9
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-15 20:30:59 +0500

    nsZoneMaster.py: extract zone master request functionality to module

--- pyScrs/nsZoneMaster.py
+++ pyScrs/nsZoneMaster.py
@@ -1,8 +1,8 @@
-import async_ssh_executor as ase
 import argparse
 import sys
 from dns import resolver, reversename, exception
-from host_lists import DNS_SERVER_LIST
+from ssh_zone_master import getDomainZoneMaster
+
 
 SERVER_LIST = "DNS"
 DNS_HOSTING_HOSTNAME = "dns.hoster.kz."
@@ -17,25 +17,7 @@ def getPtr(ip: str):
         return ip
 
 
-def getDomainZoneMaster(
-    domain_name, verbosity_flag=True, test_flag=False, debug_flag=False
-):
-    getZoneMasterCmd = f"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf| grep '\\\"{''.join(domain_name)}\\\"' | grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | head -n1"
-    dnsAnswers = []
-    dnsAnswers = ase.batch_ssh_command_result(
-        server_list=DNS_SERVER_LIST,
-        command=getZoneMasterCmd,
-        verbose=debug_flag,
-        test=test_flag,
-    )
-    if verbosity_flag:
-        return {"domain": f"{domain_name}", "answers": dnsAnswers}
 
-    unique_zone_masters = list(set(answer["stdout"] for answer in dnsAnswers))
-    return {
-        "domain": domain_name,
-        "zone_master": unique_zone_masters,
-    }
 
 
 def main():
---
+++ pyScrs/ssh_zone_master.py
@@ -0,0 +1,22 @@
+from host_lists import DNS_SERVER_LIST
+import async_ssh_executor as ase
+
+def getDomainZoneMaster(
+    domain_name, verbosity_flag=True, test_flag=False, debug_flag=False
+):
+    getZoneMasterCmd = f"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf| grep '\\\"{''.join(domain_name)}\\\"' | grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | head -n1"
+    dnsAnswers = []
+    dnsAnswers = ase.batch_ssh_command_result(
+        server_list=DNS_SERVER_LIST,
+        command=getZoneMasterCmd,
+        verbose=debug_flag,
+        test=test_flag,
+    )
+    if verbosity_flag:
+        return {"domain": f"{domain_name}", "answers": dnsAnswers}
+
+    unique_zone_masters = list(set(answer["stdout"] for answer in dnsAnswers))
+    return {
+        "domain": domain_name,
+        "zone_master": unique_zone_masters,
+    }
