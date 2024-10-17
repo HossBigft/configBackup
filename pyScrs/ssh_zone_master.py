ddb8c9374d2d342f3bee1011ca81fe0b182a3b25
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-17 21:55:15 +0500

    ssh_zone_master.py: move input validation to function

--- pyScrs/ssh_zone_master.py
+++ pyScrs/ssh_zone_master.py
@@ -7,13 +7,15 @@ DOMAIN_REGEX_PATTERN = (
 )
 
 
+def is_valid_domain(domain_name: str) -> bool:
+    return 3 <= len(domain_name) <= 63 and bool(
+        re.match(DOMAIN_REGEX_PATTERN, domain_name)
+    )
+
+
 def getDomainZoneMaster(domain_name: str, verbosity_flag=True, debug_flag=False):
-    if (
-        len(domain_name) < 3
-        or len(domain_name) > 63
-        or not bool(re.match(DOMAIN_REGEX_PATTERN, domain_name))
-    ):
-        raise ValueError("Input string should be domain name")
+    if not is_valid_domain(domain_name):
+        raise ValueError("Input string should be a valid domain name.")
 
     getZoneMasterCmd = f"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf| grep '\\\"{''.join(domain_name)}\\\"' | grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | head -n1"
     dnsAnswers = []
