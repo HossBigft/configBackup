7fe5372b68203a3c690d49b7c2d5c981dc37a464
Maksim Gluwenko <maksim_gluwenko@epam.com>
2024-10-17 21:23:03 +0500

    ssh_zone_master.py: add input validation

--- pyScrs/ssh_zone_master.py
+++ pyScrs/ssh_zone_master.py
@@ -1,10 +1,20 @@
 from host_lists import DNS_SERVER_LIST
 import async_ssh_executor as ase
+import re
+
+DOMAIN_REGEX_PATTERN = (
+    r"^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"
+)
 
 
-def getDomainZoneMaster(
-    domain_name, verbosity_flag=True, test_flag=False, debug_flag=False
-):
+def getDomainZoneMaster(domain_name: str, verbosity_flag=True, debug_flag=False):
+    if (
+        len(domain_name) < 3
+        or len(domain_name) > 63
+        or not bool(re.match(DOMAIN_REGEX_PATTERN, domain_name))
+    ):
+        raise ValueError("Input string should be domain name")
+
     getZoneMasterCmd = f"cat /var/opt/isc/scls/isc-bind/zones/_default.nzf| grep '\\\"{''.join(domain_name)}\\\"' | grep -Po '((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\\b){{4}}' | head -n1"
     dnsAnswers = []
     dnsAnswers = ase.batch_ssh_command_result(
